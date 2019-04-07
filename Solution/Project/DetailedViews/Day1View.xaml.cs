6c8dca391fd68a8ede8d3996da530249b3bc5be9
Aleksandar Nedaković <aleksaleks123@gmail.com>
2019-04-07 11:03:40 +0200

    Added plot and auto-complete textbox

---
+++ Solution/Project/DetailedViews/Day1View.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Project.DetailedViews
+{
+    /// <summary>
+    /// Interaction logic for Day1View.xaml
+    /// </summary>
+    public partial class Day1View : Window
+    {
+        public Day1View()
+        {
+            InitializeComponent();
+        }
+    }
+}
